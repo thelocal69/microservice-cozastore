d404abb749789bd53680a4fe6a8db03c21903c0b
thelocal69 <trankhanh740@gmail.com>
2024-03-24 05:39:46 +0700

    update code

--- install.bat
+++ install.bat
@@ -6,16 +6,16 @@ set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_291
 set PATH=%JAVA_HOME%\bin;%PATH%
 
 :: Set the path to the JAR files
-start java -jar discovery-server/target/discovery-server-1.0.0-SNAPSHOT.jar
-start java -jar api-gateway/target/api-gateway-1.0.0-SNAPSHOT.jar
-start java -jar security-service/target/security-service-1.0.0-SNAPSHOT.jar
-start java -jar cart-service/target/cart-service-1.0.0-SNAPSHOT.jar
-start java -jar product-service/target/product-service-1.0.0-SNAPSHOT.jar
-start java -jar user-service/target/user-service-1.0.0-SNAPSHOT.jar
-start java -jar order-service/target/order-service-1.0.0-SNAPSHOT.jar
-start java -jar media-service/target/media-service-1.0.0-SNAPSHOT.jar
-start java -jar carousel-service/target/carousel-service-1.0.0-SNAPSHOT.jar
-start java -jar blog-service/target/blog-service-1.0.0-SNAPSHOT.jar
+start java -jar discovery-server/target/discovery-server-0.0.1-SNAPSHOT.jar
+start java -jar api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar
+start java -jar security-service/target/security-service-0.0.1-SNAPSHOT.jar
+start java -jar cart-service/target/cart-service-0.0.1-SNAPSHOT.jar
+start java -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar
+start java -jar user-service/target/user-service-0.0.1-SNAPSHOT.jar
+start java -jar order-service/target/order-service-0.0.1-SNAPSHOT.jar
+start java -jar media-service/target/media-service-0.0.1-SNAPSHOT.jar
+start java -jar carousel-service/target/carousel-service-0.0.1-SNAPSHOT.jar
+start java -jar blog-service/target/blog-service-0.0.1-SNAPSHOT.jar
 
 :: Wait for all processes to finish
 timeout /t 5
--- install.sh
+++ install.sh
@@ -6,16 +6,16 @@ JAVA_HOME=/usr/bin/java
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Set the path to the JAR files
-java -jar discovery-server/target/discovery-server-1.0.0-SNAPSHOT.jar &
-java -jar api-gateway/target/api-gateway-1.0.0-SNAPSHOT.jar &
-java -jar security-service/target/security-service-1.0.0-SNAPSHOT.jar &
-java -jar cart-service/target/cart-service-1.0.0-SNAPSHOT.jar &
-java -jar product-service/target/product-service-1.0.0-SNAPSHOT.jar &
-java -jar user-service/target/user-service-1.0.0-SNAPSHOT.jar &
-java -jar order-service/target/order-service-1.0.0-SNAPSHOT.jar &
-java -jar media-service/target/media-service-1.0.0-SNAPSHOT.jar &
-java -jar carousel-service/target/carousel-service-1.0.0-SNAPSHOT.jar &
-java -jar blog-service/target/blog-service-1.0.0-SNAPSHOT.jar &
+java -jar discovery-server/target/discovery-server-0.0.1-SNAPSHOT.jar &
+java -jar api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar &
+java -jar security-service/target/security-service-0.0.1-SNAPSHOT.jar &
+java -jar cart-service/target/cart-service-0.0.1-SNAPSHOT.jar &
+java -jar product-service/target/product-service-0.0.1-SNAPSHOT.jar &
+java -jar user-service/target/user-service-0.0.1-SNAPSHOT.jar &
+java -jar order-service/target/order-service-0.0.1-SNAPSHOT.jar &
+java -jar media-service/target/media-service-0.0.1-SNAPSHOT.jar &
+java -jar carousel-service/target/carousel-service-0.0.1-SNAPSHOT.jar &
+java -jar blog-service/target/blog-service-0.0.1-SNAPSHOT.jar &
 
 # Wait for all processes to finish
 wait
